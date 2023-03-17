cc46fd9787e748c9373ecec9afda5a6da5a683a9
goagain <remain.goagain@gmail.com>
2023-03-16 18:09:30 -0700

    Update readme_bot.py

--- bin/readme_bot.py
+++ bin/readme_bot.py
@@ -20,3 +20,6 @@ def main():
     new_version = input("input a new SEMI_VERSION")
     old_version = extract_version_from_readme("./README.md")
     replace_versions("./README.md", old_version, new_version)
+    
+if __name__=="__main__":
+    main()
